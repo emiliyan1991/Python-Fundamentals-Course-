698905274bd72a4f8d9f6b0a81d8ca8e074cc395
emiliyan1991 <82729592+emiliyan1991@users.noreply.github.com>
2022-04-02 13:48:50 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops Exs/04_double_char.py
@@ -0,0 +1,6 @@
+word = input()
+double_word = ""
+for i in range(len(word)):
+    double_word += word[i]
+    double_word += word[i]
+print(double_word)
---
+++ Basic Syntax, Conditional Statements and Loops Exs/05_cant_sleep.py
@@ -0,0 +1,4 @@
+number = int(input())
+phrase = ''
+for i in range(1, number+1):
+    print((f"{i} sheep..."), end="")
