6a4ec5b98baf65f8dd5f18d2aef75f680e0f1bd9
emiliyan1991 <82729592+emiliyan1991@users.noreply.github.com>
2022-06-21 12:00:32 +0300

    Delete 06. Sort.py

--- Functions/06. Sort.py
+++
@@ -1,9 +0,0 @@
-# result = sorted(list(map(int, input().split(" "))))
-# print(result)
-
-def sorted_nums(number):
-    return sorted(number)
-
-
-result = sorted_nums(list(map(int, input().split(" "))))
-print(result)
